14bd502bac030c54c07bc00e45d00ff557acd17d
coderj001 <amirajubolchi001@gmail.com>
2021-07-19 10:31:05 +0530

    user serializers and views added

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from user.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'email',
+        'username',
+        'user_type',
+        'date_joined'
+    )
+    list_filter = (
+        'user_type',
+        'date_joined'
+    )
